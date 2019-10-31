a6df501313a3e6440799c80bdaa1e3f4e4258ecc
Frank Seitz <fs@fseitz.de>
2019-10-31 12:19:07 +0100

    Savepoint

--- lib/Sdoc/Core/System.pm
+++ lib/Sdoc/Core/System.pm
@@ -5,7 +5,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.162';
+our $VERSION = '1.163';
 
 use Sdoc::Core::Shell;
 use Sdoc::Core::FileHandle;
@@ -308,7 +308,7 @@ sub searchProgram {
 
 =head1 VERSION
 
-1.162
+1.163
 
 =head1 AUTHOR
 
