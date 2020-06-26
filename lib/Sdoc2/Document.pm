24eab2f316cc953863cca68348defc4172bb05b9
Frank Seitz <fs@fseitz.de>
2020-06-26 20:25:45 +0200

    Savepoint

--- lib/Sdoc2/Document.pm
+++ lib/Sdoc2/Document.pm
@@ -199,7 +199,8 @@ sub new {
         my $program = $p->basePath($inp).".$ext";
         if (-e $program) {
             (my $dir,$program) = $p->split($program);
-            Sdoc::Core::Shell->exec("(cd $dir; ./$program)");
+            my $sh = Sdoc::Core::Shell->new(log=>1);
+            $sh->exec("(cd $dir; ./$program)");
         }
     }
 
