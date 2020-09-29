ca9cc96b1d90796be0de595853c9090b3d7b510c
Frank Seitz <fs@fseitz.de>
2020-09-29 20:05:43 +0200

    Savepoint

--- lib/Sdoc/Core/Html/Tag.pm
+++ lib/Sdoc/Core/Html/Tag.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.187';
+our $VERSION = '1.189';
 
 use Sdoc::Core::Css;
 use Sdoc::Core::Template;
@@ -2178,7 +2178,7 @@ sub import {
 
 =head1 VERSION
 
-1.187
+1.189
 
 =head1 AUTHOR
 
--- lib/Sdoc/Core/MediaWiki/Markup.pm
+++ lib/Sdoc/Core/MediaWiki/Markup.pm
@@ -6,7 +6,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.187';
+our $VERSION = '1.189';
 
 use Sdoc::Core::Unindent;
 use Sdoc::Core::Parameters;
@@ -1823,7 +1823,7 @@ sub testPage {
 
 =head1 VERSION
 
-1.187
+1.189
 
 =head1 AUTHOR
 
