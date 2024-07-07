a64b77d9b79a9ac0fda3c096b1f2310a4b997d58
Frank Seitz <fs@fseitz.de>
2024-07-07 22:00:04 +0200

    Savepoint

--- lib/Sdoc/Core/Duration.pm
+++ lib/Sdoc/Core/Duration.pm
@@ -70,7 +70,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.217';
+our $VERSION = '1.218';
 
 use Sdoc::Core::Option;
 use Sdoc::Core::Math;
@@ -520,7 +520,7 @@ sub secondsToString {
 
 =head1 VERSION
 
-1.217
+1.218
 
 =head1 AUTHOR
 
--- lib/Sdoc/Core/Ipc.pm
+++ lib/Sdoc/Core/Ipc.pm
@@ -21,7 +21,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.217';
+our $VERSION = '1.218';
 
 use Sdoc::Core::Option;
 use Sdoc::Core::Shell;
@@ -189,7 +189,7 @@ sub pipeTo {
 
 =head1 VERSION
 
-1.217
+1.218
 
 =head1 AUTHOR
 
