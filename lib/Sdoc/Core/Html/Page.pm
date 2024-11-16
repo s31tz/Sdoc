cafcec984832813d0074c1fd8acf6a621317443c
Frank Seitz <fs@fseitz.de>
2024-11-16 18:00:06 +0100

    Savepoint

--- lib/Sdoc/Core/Html/Page.pm
+++ lib/Sdoc/Core/Html/Page.pm
@@ -136,7 +136,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.221';
+our $VERSION = '1.222';
 
 use Sdoc::Core::Html::Component::Bundle;
 use Sdoc::Core::Css;
@@ -304,7 +304,7 @@ sub html {
 
 =head1 VERSION
 
-1.221
+1.222
 
 =head1 AUTHOR
 
--- lib/Sdoc/Core/Parameters.pm
+++ lib/Sdoc/Core/Parameters.pm
@@ -21,7 +21,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '1.221';
+our $VERSION = '1.222';
 
 use Sdoc::Core::Converter;
 use Sdoc::Core::Hash;
@@ -481,7 +481,7 @@ sub extractToObject {
 
 =head1 VERSION
 
-1.221
+1.222
 
 =head1 AUTHOR
 
