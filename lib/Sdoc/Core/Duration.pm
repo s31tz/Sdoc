d983c61ff2ac353b6a52eb50bd9faa6b3d509c88
Frank Seitz <fs@fseitz.de>
2025-10-22 12:00:04 +0200

    Savepoint

--- lib/Sdoc/Core/Duration.pm
+++ lib/Sdoc/Core/Duration.pm
@@ -70,7 +70,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.231';
+our $VERSION = '1.232';
 
 use Sdoc::Core::Option;
 use Sdoc::Core::Math;
@@ -520,7 +520,7 @@ sub secondsToString {
 
 =head1 VERSION
 
-1.231
+1.232
 
 =head1 AUTHOR
 
