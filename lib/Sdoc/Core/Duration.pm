ad6324860bc68bc5abc95ec7f4a713a37f5a975f
Frank Seitz <fs@fseitz.de>
2026-03-08 12:07:58 +0100

    Savepoint

--- lib/Sdoc/Core/Duration.pm
+++ lib/Sdoc/Core/Duration.pm
@@ -70,7 +70,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.235';
+our $VERSION = '1.236';
 
 use Sdoc::Core::Option;
 use Sdoc::Core::Math;
@@ -520,7 +520,7 @@ sub secondsToString {
 
 =head1 VERSION
 
-1.235
+1.236
 
 =head1 AUTHOR
 
--- lib/Sdoc/Core/MediaWiki/Markup.pm
+++ lib/Sdoc/Core/MediaWiki/Markup.pm
@@ -63,7 +63,7 @@ use strict;
 use warnings;
 use utf8;
 
-our $VERSION = '1.235';
+our $VERSION = '1.236';
 
 use Sdoc::Core::Unindent;
 use Sdoc::Core::Parameters;
@@ -1829,7 +1829,7 @@ sub testPage {
 
 =head1 VERSION
 
-1.235
+1.236
 
 =head1 AUTHOR
 
